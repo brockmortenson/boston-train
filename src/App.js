b50a74461d47e11a414b44732d7062e931d9d4d3
Brock Mortenson <mortensonbrock@gmail.com>
2021-05-30 21:48:40 -0600

    Still unable to get departure times rendered. I will work more on it tomorrow.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ schedule, setSchedule ] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://api-v3.mbta.com/schedules?filter%5Broute%5D=filter%5B0%2C1%2C2%5D')
+      .then(res => {
+        console.log(res.data)
+        setSchedule(res.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  // const scheduleArr = Object.keys(schedule);
+  // console.log(scheduleArr)
+  // const arr = Object.keys(scheduleArr)
+  // console.log(arr)
+
+  const mappedSchedule = schedule.data.map((schedules, index) => {
+    console.log(schedules)
+    return (
+      <div key={index}>
+        <div>{schedules}</div>
+      </div>
+    )
+  })
+
+  console.log(mappedSchedule)
+
+  // .data[0].attributes.departure_time
+
+  // route.data[attributes].departure_time
+  // curl -X GET "https://api-v3.mbta.com/schedules?sort=departure_time&include=prediction&filter%5Bdate%5D=filter%5B2021-05-28&filter%5Broute_type%5D=1" -H "accept: application/vnd.api+json"
+  // https://api-v3.mbta.com
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
+      {mappedSchedule}
     </div>
   );
 }
